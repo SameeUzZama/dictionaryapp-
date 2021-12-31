7f267e69260bc0a8e8f29c4b4713a79fe085194a
Samee Uz Zama <samiuzzama619@gmail.com>
2021-12-31 22:22:37 +0530

    dictionary

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import DictionaryApp from "./Components/DictionaryApp";
+
+function App() {
+  return (
+    <div className="App">
+      <DictionaryApp />
+    </div>
+  );
+}
+
+export default App;
